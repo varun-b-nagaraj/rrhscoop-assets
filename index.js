d20ba594b9ee3ad5da7e76b2fbecae8a82aa9099
(Personal) Varun Bhadurgatte Nagaraj <varunnagaraj2009@gmail.com>
2026-08-23 13:14:24 -0500

    remove some changes

--- index.js
+++ index.js
@@ -22,7 +22,7 @@
 
   const base = baseForCurrentScript();
   const runtime = new URL('rrhs-runtime.js', base).href;
-  const visuals = new URL('custom-visuals/rrhscoop-visuals.js?v=39', base).href;
+  const visuals = new URL('custom-visuals/rrhscoop-visuals.js?v=38', base).href;
   const headerDropdowns = new URL('custom-visuals/rrhs-header-dropdowns.js?v=8', base).href;
   const faqStickyNav = new URL('custom-visuals/rrhs-faq-sticky-nav.js?v=5', base).href;
   const projectDeepLinks = new URL('custom-visuals/rrhs-project-deeplinks.js?v=7', base).href;
